a4cf478c2ad432393408d9c33ac15928481e268c
pontoreau <pontoreau.sylvain@gmail.com>
2015-02-02 23:15:36 +0100

    Replace Xaml with C# code !

---
+++ src/XamarinChat/Page/ConnectPage.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace XamarinChat
+{
+	public class ConnectPage : ContentPage
+	{
+		public ConnectPage()
+		{
+			InitializeComponent();
+			BindingContext = ViewModelFactory.Get<ConnectViewModel>();
+		}
+
+		public void InitializeComponent()
+		{
+			var layout = new StackLayout
+			{
+				VerticalOptions = LayoutOptions.Center
+			};
+
+			var textBox = new Entry 
+			{ 
+				Placeholder = "Name" 
+			};
+			textBox.SetBinding(Entry.TextProperty, "ClientName");
+
+			var label = new Label
+			{
+				XAlign = TextAlignment.Center,
+				BindingContext = textBox
+			};
+			label.SetBinding<Entry>(Label.TextProperty, t => t.Text , BindingMode.Default, null, "Hello {0} !");
+
+			var button = new Button
+			{
+				VerticalOptions = LayoutOptions.Center,
+				Text = "Connect"
+			};
+
+			layout.Children.Add(label);
+			layout.Children.Add(textBox);
+			layout.Children.Add(button);
+
+			Content = layout;
+		}
+	}
+}
+
