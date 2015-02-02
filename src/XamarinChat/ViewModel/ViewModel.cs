cc38fe2565fe439a127347f4ea64a5a68c5330e8
pontoreau <pontoreau.sylvain@gmail.com>
2015-02-02 08:05:04 +0100

    Base ViewModel & ViewModelFactory

---
+++ src/XamarinChat/ViewModel/ViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+
+namespace XamarinChat
+{
+	/// <summary>
+	/// View model.
+	/// </summary>
+	public abstract class ViewModel : INotifyPropertyChanged
+	{
+		/// <summary>
+		/// Occurs when property changed.
+		/// </summary>
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		/// <summary>
+		/// Raises the property changed event.
+		/// </summary>
+		/// <param name="propertyName">Property name.</param>
+		protected void OnPropertyChanged(string propertyName)
+		{
+			var tmp = PropertyChanged;
+
+			if(tmp != null)
+				PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+}
+
---
+++ src/XamarinChat/ViewModel/ViewModelFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace XamarinChat
+{
+	/// <summary>
+	/// View model factory.
+	/// </summary>
+	public static class ViewModelFactory
+	{
+		/// <summary>
+		/// Get an instance of T type.
+		/// </summary>
+		/// <typeparam name="T">The 1st type parameter.</typeparam>
+		public static T Get<T>() where T : INotifyPropertyChanged
+		{
+			return Host.Get<T>();
+		}
+	}
+}
+
